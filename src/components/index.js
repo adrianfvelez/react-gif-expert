21b7eccaf9e2b69f58515fee0ef8430d25d735bf
Adrián Felipe Vélez Rivera <adrian.velez@mejoredu.gob.mx>
2024-01-24 13:12:15 -0600

    Primer commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// export { AddCategory } from './AddCategory';
+// export { GifGrid } from './GifGrid';
+// export { GifItem } from './GifItem';
+export * from './AddCategory';
+export * from './GifGrid';
+export * from './GifItem';
